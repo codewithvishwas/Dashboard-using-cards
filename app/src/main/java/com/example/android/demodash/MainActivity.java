3b95569e61801144253db1f67584066dae3077be
codewithvishwas <blckcodr@gmail.com>
2019-03-01 22:54:15 +0530

    dashboard using cards with animation

---
+++ app/src/main/java/com/example/android/demodash/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.demodash;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+
+        // dashboard instantiate
+
+        dashboard dashboard = new dashboard();
+        mFragmentTransaction.replace(R.id.container, dashboard);
+        mFragmentTransaction.commit();
+    }
+}
